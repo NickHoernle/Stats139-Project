d8a18527a31ced84f9fc68cdde5e70bae31bf2a2
Nick Hoernle <nick@intellectionsoftware.com>
2016-12-02 16:13:18 -0500

    cleaned data script updated, saved cleaned data files, started Kimia analysis script

---
+++ analysis.R
@@ -0,0 +1,7 @@
+# read in the clean data
+demographics <- read.csv('./data/clean_demographics.csv')
+edu_df <- read.csv('./data/clean_education_data.csv')
+
+# visulisations, variable transformations etc
+# Kimia you can start exploring here
+pairs(demographics)
